14a425e69f6f916789c347110a1d274f1baa1173
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2022-06-02 21:09:12 +0200

    add

--- src/page/Details/Details.jsx
+++ src/page/Details/Details.jsx
@@ -1,55 +1,56 @@
-import { 
-    Text, View, Button, VStack,
+import {
+    Text,
+    View,
+    Button,
+    VStack,
     Image,
     Box,
     Heading,
     Center,
-    ScrollView, 
+    ScrollView,
 } from "native-base";
 
 import { StyleSheet } from "react-native";
 
-const Details = ({navigation, route}) => {
+const Details = ({ navigation, route }) => {
+    const { data } = route.params;
+    return (
+        <Center>
+            <VStack>
+                <ScrollView>
+                    <Box alignItems="center">
+                        <Center>
+                            <Heading mt={5} marginBottom={2} size={"xl"}>
+                                {data.name}
+                            </Heading>
+                        </Center>
+                        <Image
+                            source={{
+                                uri: `https://picsum.photos/id/${
+                                    data.id + 29
+                                }/200/300`,
+                            }}
+                            alt="Details"
+                            size="xl"
+                            width="98%"
+                            height="400px"
+                            borderRadius={10}
+                        />
 
-    const {data} = route.params;
-    return <Center>
-        <VStack>
-            <ScrollView>
-                <Box
-                    alignItems="center"
-                >
-                    <Center>
-                        <Heading
-                            mt={5}
-                            marginBottom={2}
-                            size={"xl"}
-                        >
-                            {data.name}
-                        </Heading>
-                    </Center>
-                    <Image 
-                        source={{
-                            uri: `https://picsum.photos/id/${data.id + 29}/200/300`,
-                        }} 
-                        alt="Details" 
-                        size="xl" 
-                        width="98%"
-                        height="400px"
-                        borderRadius={10}
-                    />
-
-                    <Center
-                        marginTop={5}
-                    >
-                        <Heading size="lg">@{data.restaurant}</Heading>
-                    </Center>
+                        <Center marginTop={5}>
+                            <Heading size="lg">@{data.restaurant}</Heading>
+                        </Center>
                         <Box
                             paddingLeft={1}
                             textAlign="justify"
                             marginBottom={10}
                         >
                             <Text>{data.description}</Text>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni odit dolore praesentium nobis modi quam consequuntur accusamus beatae possimus. Odit ducimus autem molestias et voluptates non itaque assumenda cum!
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Voluptatem magni odit dolore praesentium nobis
+                            modi quam consequuntur accusamus beatae possimus.
+                            Odit ducimus autem molestias et voluptates non
+                            itaque assumenda cum!
                         </Box>
 
                         {/* Afficher les fifférents ingrédients de la recette */}
@@ -61,32 +62,26 @@ const Details = ({navigation, route}) => {
                             marginBottom={5}
                         >
                             <Heading size="md">Ingredients</Heading>
-                            <Box
-                                textAlign="justify"
-                                marginBottom={10}
-                            >
+                            <Box textAlign="justify" marginBottom={10}>
                                 <Text>{data.ingredients}</Text>
                             </Box>
                         </Box>
-                        
 
-                    <Button
-                        // position={'relative'}
-                        shadow={2}
-                        onPress={()=>navigation.goBack()}
-                        // marginBottom={30}
-                    >
-                        Retourner à l'accueil
-                    </Button>
-                </Box>
-            </ScrollView>
-        </VStack>
-    </Center>
-    
-}
+                        <Button
+                            // position={'relative'}
+                            shadow={2}
+                            onPress={() => navigation.goBack()}
+                            // marginBottom={30}
+                        >
+                            Retourner à l'accueil
+                        </Button>
+                    </Box>
+                </ScrollView>
+            </VStack>
+        </Center>
+    );
+};
 
-const styles = StyleSheet.create({
-    
-});
+const styles = StyleSheet.create({});
 
-export default Details
+export default Details;
